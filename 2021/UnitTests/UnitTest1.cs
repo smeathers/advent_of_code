5ce3312e77774c5b8c5f03aeaf05d8a7bad2bd7c
Edward <edward@smeathers.co.uk>
2023-01-24 20:09:16 +0000

    Merging of repos

---
+++ 2021/UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Day01;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Day01Part1()
+        {
+        }
+    }
+}
