687f81ca15a65dac77d89c621a31f90c2bbb01f4
Edward <Edward@EVESHAM>
2023-11-10 22:08:37 +0000

    Day 2 and start of unit tests

--- 2022/Day01/Program.cs
+++ 2022/Day01/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 // https://adventofcode.com/2022/day/1
 // Part1 = 66306
-// Part2 = 
+// Part2 = 195292
 
 namespace Day01
 {
@@ -13,11 +14,11 @@ namespace Day01
         static void Main()
         {
             var lines = File.ReadLines("input.txt");
-            int answer = Part1(lines);
+            int answer = Part2(lines);
             Console.WriteLine(answer);
         }
 
-        static int Part1(IEnumerable<string> lines)
+        static public int Part1(IEnumerable<string> lines)
         {
 
             int number = 1;
@@ -28,21 +29,52 @@ namespace Day01
             foreach (var line in lines)
             {
                 if (int.TryParse(line, out int parsed))
-                {
-                    //Console.WriteLine("Line: " + line);
                     count += parsed;
-                }
                 else
-                {
                     process(ref number, ref count, ref bestCount, ref bestNumber);
-                }
             }
 
             process(ref number, ref count, ref bestCount, ref bestNumber);
 
             Console.WriteLine("BestNumber: " + bestNumber);
             Console.WriteLine("BestCount: " + bestCount);
+
             return bestCount;
+        }
+
+        static public int Part2(IEnumerable<string> lines)
+        {
+            //List<int> output;
+            var output = new List<int>();
+            int current = 0;
+            int count = 3;
+
+            foreach (var line in lines)
+            {
+                if (int.TryParse(line, out int parsed))
+                    current += parsed;
+                else
+                {
+                    output.Add(current);
+                    current = 0;
+                }
+            }
+            if (current != 0)
+                output.Add(current);
+
+            Console.WriteLine();
+            output = output.OrderByDescending(i => i).ToList();
+
+            foreach (var item in output)
+            {
+                Console.WriteLine(item);
+            }
+
+            int total = 0;
+            for (int i = 0; i < count; i++)
+                total += output[i];
+
+            return total;
         }
 
         static void process(ref int number, ref int count, ref int bestCount, ref int bestNumber)
